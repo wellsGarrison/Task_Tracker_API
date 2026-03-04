2ea88bebb3e0bb151887b0c8fed69aeb53129c41
Garrison Wells <gwells1@umd.edu>
2026-03-04 13:50:17 -0500

    commented app.py

--- README.md
+++ README.md
@@ -2,4 +2,15 @@
 
 API to allow users to manage their to-do list. 
 
+Provides user registration, login, and authentication through JWT tokens. CRUD operations for tasks. Users and tasks stored in a PostgreSQL database interfaced with through psycopg2. 
+
+Built with Flask and psycopg2. Additional libraries such as Argon2, PyJWT, jsonschema used for user authentication, data validation, etc. 
+
+Future additions: 
+ - Redis cache for revoked tokens 
+ - Move some logic into separate directories and files. For example a directory for routes and separate files for user and todo list functions. 
+ - Change encryption scheme to RS256
+
+
+
 From: https://roadmap.sh/projects/todo-list-api 
--- src/app.py
+++ src/app.py
@@ -1,11 +1,10 @@
 from flask import Flask, request, jsonify, g
 from database.Data import DataHandler
-# from schemas import *
 import schemas
-# from pymongo import MongoClient
 import psycopg2
 from jsonschema import validate, ValidationError
 from argon2 import PasswordHasher
+from argon2.exceptions import VerifyMismatchError
 from datetime import datetime, timezone
 import os
 from dotenv import load_dotenv
@@ -13,9 +12,8 @@ import jwt
 from uuid import uuid7
 from jwt.exceptions import ExpiredSignatureError
 from functools import wraps
-from argon2.exceptions import VerifyMismatchError
 import config
-from Task.Task import Task, Status
+from Task.Task import Status
 
 
 app = Flask(__name__)
@@ -23,14 +21,17 @@ dh = None
 ph = None
 load_dotenv()
 
+# Wrapper for validating session tokens 
 def token_required(f):
     @wraps(f)
     def check_token(*args, **kwargs):
+        # Authentication done with JWT stored in the authorization header
         auth = request.authorization
         if auth and auth.type == 'bearer':
             token = auth.token
-            # Validate token logic...
             try:
+                # use PyJWT to decode token, token includes user_id and expiration timestamp
+                # store user_id in the flask.g object so it can be accessed in other methods 
                 claim = jwt.decode(token, key=os.getenv('JWT_secret'), algorithms=['HS256', ])
                 g.user_id = claim['sub']
             except ExpiredSignatureError as e:
@@ -44,27 +45,28 @@ def token_required(f):
         
     return check_token
 
+# User registration route, return authentication token if registered succesfully, error otherwise 
 @app.route("/create_account", methods=["POST"])
 def create_account():
     data = request.get_json()
     
+    # Validate JSON
     try:
         validate(instance=data, schema=schemas.user_registration)
-        # print("Valid data conforms to the schema.")
     except ValidationError as e:
-        print(f"Validation Error for valid_data: {e.message}")
+        print(f"Validation Error for user_registration: {e.message}")
         return jsonify(data), 400
     
     # hash password and generate user_id
     hash = ph.hash(data['password'])
     user_id = str(uuid7())
 
-    # generate token 
-    # exp = datetime.now(tz=timezone.utc).timestamp() + 60*5
+    # generate token, token_timeout measured in seconds
     jwt_payload = {
         "sub": user_id,
         "exp": datetime.now(tz=timezone.utc).timestamp() + config.token_timeout
     }
+    # encode using HS256
     secret = os.getenv('JWT_secret')
     token = jwt.encode(
         payload=jwt_payload,
@@ -77,7 +79,6 @@ def create_account():
     except psycopg2.DatabaseError as e:
         if str(e).startswith('duplicate key value violates unique constraint "unique_email"'):
             return jsonify({"message": "email already in use"}), 409
-        # print(e.args[0])
         else:
             print(e)
             return jsonify({"message": "database error"}), 500
@@ -85,21 +86,19 @@ def create_account():
     # return token if user registered, return error otherwise 
     return jsonify({"token": token}), 201
 
-
+# Login route, return authentication token if crednetials are valid, error otherwise 
 @app.route("/login", methods=["POST"])
 def login():
     data = request.get_json()
 
+    # Validate JSON
     try:
         validate(instance=data, schema=schemas.user_login)
-        # print("Valid data conforms to the schema.")
     except ValidationError as e:
-        print(f"Validation Error for data: {e.message}")
+        print(f"Validation Error for user_login: {e.message}")
         return jsonify(data), 400
     
-    # TODO 
-    # get salt and hash and compare with password 
-    # print(data['email'])
+    # get salt and hash (Argon2 hash contains hash and salt)
     email = data['email']
     password = data['password']
     try:
@@ -110,6 +109,7 @@ def login():
         print(f"Unexpected {e=}, {type(e)=}")
         return jsonify({'message': 'database error'}), 500
 
+    # Validate password against hash
     try:
         ph.verify(hash, password)
     except VerifyMismatchError as e:
@@ -118,8 +118,7 @@ def login():
         print(f"Unexpected {e=}, {type(e)=}")
         return jsonify({'message': 'incorrect password'}), 401
     
-    # return token or error 
-    # generate token 
+    # Get user_id and regenerate hash if necessary 
     try:
         user_id = dh.get_user_id(email)
         if ph.check_needs_rehash(hash):
@@ -130,74 +129,80 @@ def login():
         print(f"Unexpected {e=}, {type(e)=}")
         return jsonify({'message': 'database error'}), 401
     
+    # generate token, token_timeout measured in seconds
     jwt_payload = {
         "sub": user_id,
         "exp": datetime.now(tz=timezone.utc).timestamp() + config.token_timeout
     }
+    # encode using HS256
     secret = os.getenv('JWT_secret')
     token = jwt.encode(
         payload=jwt_payload,
         key=secret
     )
 
-    # return token if user registered, return error otherwise 
+    # return token 
     return jsonify({"token": token}), 201
 
 
-# validate json, pass json to DataHandler to add task to db
+# Add task route. validate json, pass json to DataHandler to add task to db
+# Return task details with a task_id and status code 201 on success 
 @app.route("/todos", methods=["POST"])
 @token_required
 def add_task():
     data = request.get_json()
-    user_id = g.user_id
-    # TODO
-    # check authentication token     
+    user_id = g.user_id   
     
     # validate json 
     try:
         validate(instance=data, schema=schemas.create_task)
-        # print("Valid data conforms to the schema.")
     except ValidationError as e:
-        print(f"Validation Error for valid_data: {e.message}")
+        print(f"Validation Error for create_task: {e.message}")
         return jsonify(data), 400
     
-
-
-    # description = data['description']
+    # generate task_id, pass task details to data handler 
     task_id = str(uuid7())
     title  = data['title']
     desc = data['description']
     dh.add_task(user_id, task_id, title, desc)
 
+    # return task details and status code 
     return jsonify({
         'id': task_id,
         'title': title,
         'description': desc
     }), 201
 
+# Update task route. Change the status of a task or edit the title and description. 
+# return task details with task_id on success 
 @app.route("/todos/<task_id>", methods=["PUT"])
 @token_required
 def update_task(task_id):
     data = request.get_json()
     user_id = g.user_id
 
+    # validate JSON
     try:
         validate(instance=data, schema=schemas.update_task)
-        # print("Valid data conforms to the schema.")
     except ValidationError as e:
         print(f"Validation Error for update_task: {e.message}")
         return jsonify(data), 400
     
-    # task_id = data['task_id']
-    rtn = None
+    # change status or title and description depending on what was provided 
+    # rtn will be 1 if a task with the given task_id is found, 0 if not
+    # obfuscates if user doesn't have access to task or if the task doesn't exist 
+    rtn = 0
     try:
         if "status" in data.keys():
+            # validate status is TODO, INPROGRESS, or DONE
             if data['status'] in Status._member_names_:
+                # pass to data handler 
                 rtn = dh.change_status(user_id, task_id, data['status'])
             else:
                 return 'invalid status', 400
             
         elif "title" in data.keys() and "description" in data.keys():
+            # pass to data handler 
             rtn = dh.change_task(user_id, task_id, data['title'], data['description'])
 
     except psycopg2.DatabaseError as e:
@@ -209,46 +214,32 @@ def update_task(task_id):
     else:
         data['task_id'] = task_id
         return jsonify(data), 204 
-    
 
-# @app.route("/mark_task", methods=["PUT"])
-# def mark_task():
-#     data = request.get_json()  
-
-
+# Delete task route, return 204 on successful delete, error otherwise 
 @app.route("/todos/<task_id>", methods=["DELETE"])
 @token_required
 def delete_task(task_id):
-    # data = request.get_json()
     user_id = g.user_id
 
-    # try:
-    #     validate(instance=data, schema=schemas.delete_task)
-    #     # print("Valid data conforms to the schema.")
-    # except ValidationError as e:
-    #     print(f"Validation Error for valid_data: {e.message}")
-    #     return jsonify(data), 400
-
+    # pass to data handler 
+    # rtn will be 1 if a task with the given task_id is found, 0 if not
+    # obfuscates if user doesn't have access to task or if the task doesn't exist 
     rtn = dh.delete_task(user_id, task_id)
     if rtn == 0:
         return jsonify({"message": "task not found in user's tasks"}), 404
     else:
-        # data['task_id'] = task_id
         return '', 204 
 
-# /todos?page=1&limit=10
+# List tasks route, arguments for page, limit, and to filter by status. 
+# returns a dictionary with a list of tasks, the page, limit, and total returned 
 @app.route("/todos", methods=['GET'])
 @token_required
 def get_tasks():
-    # TODO: 
-    # validate input
-    # check auth token 
     user_id = g.user_id
 
     page = request.args.get('page', default=1)
     limit = request.args.get('limit', default=10)
     status = request.args.get('status', default=None)
-    print(status)
 
     lst = dh.get_tasks(user_id, status, int(page), int(limit))
 
@@ -259,9 +250,6 @@ def get_tasks():
 
 
 if __name__ == "__main__":
-    # client = MongoClient()
-    # client.admin.command('ping')
-    # print("Successfully connected to MongoDB!")
     dh = DataHandler()
     ph = PasswordHasher()
     app.run(port= 5000, debug=True)
